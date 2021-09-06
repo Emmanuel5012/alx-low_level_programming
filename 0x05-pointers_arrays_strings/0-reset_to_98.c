df2da16353d7ba348f0b867eecc851ba400277e2
Emmanuel5012 <osomisakin@gmail.com>
2021-09-06 08:24:13 -0700

    function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *reset_t0_98 - Takes a pointer to an int
+ *updates the value it points to
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reser_to_98(int *n);
+void print_rev(char *s);
+
+#endif
