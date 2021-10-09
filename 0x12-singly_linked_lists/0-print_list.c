8fb063f06ff3550ad8611c4491d1f62772094bd2
Emmanuel5012 <osomisakin@gmail.com>
2021-10-09 00:28:25 -0700

    Printing lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,14 +12,17 @@ size_t print_list(const list_t *h)
 {
 size_t nodes = 0;
 
-while(nodes != NULL){
-if(nodes->next == NULL){
-printf("%d", nodes->value);
+while (h)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+
+else
+printf("[%d] %s\n", h->len, h->str);
+
+nodes++;
+h = h->next;
 }
-else{
-printf("%d, ", nodes->value);
-}
-nodes = nodes->next;
-}
+
 return (nodes);
 }
